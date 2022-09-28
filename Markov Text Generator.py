7c9ef8c20717a2bc3e12dda4b1f30a0af8478518
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-09-27 20:49:18 -0400

    Update Markov Text Generator.py
    Updated for a better handling of n=1 situations.

--- Markov Text Generator.py
+++ Markov Text Generator.py
@@ -12,7 +12,7 @@ def finish_sentence(sentence, n, corpus, deterministic=False):
     if count == {}:
         x = n
     # Create a back-off loop for while the count dictionary is empty
-    while (count == {}) and (x > -2):
+    while (count == {}) and (x > 0):
         count = buildCount(sentence, x - 1, corpus)
         x -= 1
         pass
@@ -39,16 +39,29 @@ def finish_sentence(sentence, n, corpus, deterministic=False):
 # define loop function to allow for back-off recursion
 def buildCount(sentence, n, corpus):
     count = {}
-    # Loop over the entire corpus
-    for eachToken in range(n - 1, len(corpus)):
-        # find the n-grams that match the sentence
-        if corpus[eachToken - n + 1 : eachToken] == sentence[-n + 1 :]:
-            # Count the number of times each n-gram occurs
-            if corpus[eachToken] not in count:
-                count[corpus[eachToken]] = 1
+    # Count the frequency of each word if n is 1
+    if n == 1:
+        for eachWord in corpus:
+            if eachWord not in count:
+                count[eachWord] = 1
                 pass
             else:
-                count[corpus[eachToken]] += 1
+                count[eachWord] += 1
+                pass
+            pass
+        pass
+    else:
+        # Loop over the entire corpus
+        for eachToken in range(n - 1, len(corpus)):
+            # find the n-grams that match the sentence
+            if corpus[eachToken - n + 1 : eachToken] == sentence[-n + 1 :]:
+                # Count the number of times each n-gram occurs
+                if corpus[eachToken] not in count:
+                    count[corpus[eachToken]] = 1
+                    pass
+                else:
+                    count[corpus[eachToken]] += 1
+                    pass
                 pass
             pass
         pass
@@ -58,8 +71,8 @@ def buildCount(sentence, n, corpus):
 # define main function
 if __name__ == "__main__":
     # Test the function
-    sentence = ["how", "many", "days", "in", "a", "year"]
-    n = 2
+    sentence = ["how", "many", "days", "in", "a"]
+    n = 5
     corpus = nltk.word_tokenize(nltk.corpus.gutenberg.raw("austen-sense.txt").lower())
     deterministic = False
     while (sentence[-1] not in [".", "!", "?"]) and (len(sentence) < 10):
